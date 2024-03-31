846c71864c7dda52794c4722c196c063d728c4df
arif jayadi <arifjayadi@DESKTOP-IK4HH06>
2024-03-31 12:46:31 +0700

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui ipsa ullam
+        fugiat, vitae doloribus iusto ratione placeat voluptate autem nihil fuga
+        voluptates repellat consequuntur. Soluta dolor ipsam exercitationem odio
+        inventore!
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem magnam
+        praesentium ratione esse quia totam molestiae nam ipsa consectetur!
+        Eligendi blanditiis quod debitis quos quae sit aspernatur exercitationem
+        rerum nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Reiciendis repellendus dicta non debitis fugiat inventore. Qui
+        cupiditate unde enim porro iste aliquam iure dolore assumenda, minima
+        quaerat omnis, dolorum veniam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laudantium
+        consectetur eligendi adipisci iusto quae enim illum vitae magni
+        architecto, rerum hic, consequuntur voluptates, fugiat debitis
+        voluptatum rem at ipsum! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Esse voluptates dignissimos odio rem unde doloremque,
+        quibusdam suscipit sequi pariatur dolores. Sint repellat cupiditate
+        voluptatem voluptas obcaecati amet rem accusantium animi.
+      </p>
+    </div>
+  );
+}
